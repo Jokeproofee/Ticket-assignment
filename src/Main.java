6bd56053a30243c49d26625e7c571e2e4d49e173
Proofee <proofee@mail.ru>
2022-10-06 16:39:28 +0300

    Практическая часть по билету №4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание по билету
+        System.out.println("Задание по билету");
+        String[] someArray = {"Hello","World","Sarah","Hello","World","John","Hello","World","Carl"};
+        someArray = new LinkedHashSet<>(Arrays.asList(someArray)).toArray(new String[0]);
+        String array = Arrays.toString(someArray);
+        System.out.println(array);
     }
 }
